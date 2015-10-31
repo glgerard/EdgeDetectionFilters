19ab6b5d92b1f5d24ef9f5eff4802837e761d577
Gianluca Gerard <glgerard@gmail.com>
2015-10-31 12:28:34 +0100

    Initial Commit

---
+++ EdgeFilters/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  EdgeFilters
+//
+//  Created by Gianluca Gerard on 31/10/15.
+//  Copyright © 2015 Gianluca Gerard. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
